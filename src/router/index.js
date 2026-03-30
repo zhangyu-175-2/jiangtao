9ce225f55b7a2eb9ba21ba3a9f9054a60bb70a4a
zhangyu-175-2 <3821604502@qq.com>
2026-03-30 17:21:48 +0800

    fix: 修复线上异常

--- src/router/index.js
+++ src/router/index.js
@@ -164,7 +164,7 @@ const routes = [
 
 const router = createRouter({
   // history: createWebHistory(),
-  history: createWebHashHistory(),
+  history: createWebHashHistory("/jiangtao/"),
   routes: routes,
   scrollBehavior(to, from, savedPosition) {
     return { left: 0, top: 0 }
